9fc9a3fdc05aef76d5b5d6ce4f52cbe8340fc402
Mustakimokter <mustakimmokter@gmail.com>
2023-10-14 14:08:35 +0600

    Provider practice

--- lib/features/home/ui/screen/home_screen.dart
+++ lib/features/home/ui/screen/home_screen.dart
@@ -1,86 +1,179 @@
-import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
-import 'package:phone_contact_app/features/create/ui/screen/create_screen.dart';
-import 'package:phone_contact_app/features/favorite/ui/screen/favorite_screen.dart';
-import 'package:phone_contact_app/features/home/provider/navbar_provider.dart';
-import 'package:phone_contact_app/features/home/ui/component/index.dart';
-import 'package:phone_contact_app/shared/services/user_services/auth_service.dart';
+import 'package:phone_contact_app/features/home/provider/body_provider.dart';
+import 'package:phone_contact_app/shared/services/user_services/user.dart';
 import 'package:phone_contact_app/shared/utils/index.dart';
+import 'package:phone_contact_app/shared/utils/size_utils.dart';
 import 'package:phone_contact_app/shared/widgets/index.dart';
+import 'package:phone_contact_app/shared/widgets/search_bar.dart';
 import 'package:provider/provider.dart';
 
-
-// ignore: must_be_immutable
-class HomeScreen extends StatelessWidget {
-  HomeScreen({Key? key}) : super(key: key);
-
-  final TextEditingController numberCNTLR = TextEditingController();
-  final TextEditingController nameCNTLR = TextEditingController();
-
-  List<Widget> body = [
-    const HomeScreenBody(),
-    const FavoriteScreen(),
-    CreateScreen(),
-  ];
-
+class HomeScreenBody extends StatelessWidget {
+  const HomeScreenBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final navbarProvider = Provider.of<NavbarProvider>(context);
-    SizeUtils().init(context);
+    final bodyProvider = Provider.of<BodyProvider>(context);
+    final userService = Provider.of<UserService>(context);
     return Scaffold(
-      backgroundColor: Colors.white,
-      //resizeToAvoidBottomInset: false,
-      body: ListenableProvider(create: (context) => AuthService(),
-      child: PageTransitionSwitcher(
-        transitionBuilder: (child, primaryAnimation, secondaryAnimation) {
-          return FadeThroughTransition(animation: primaryAnimation, secondaryAnimation: secondaryAnimation,child: child);
-        },
-        child: body[navbarProvider.selectedIndex],
-      ),
-      ),
-      drawer: CustomContainer(
-        width: SizeUtils.getProportionateScreenWidth(265),
-        radius: 0,
-        color: Colors.white,
-        child: const CustomDrawer(),
-      ),
-      bottomNavigationBar: CustomContainer(
-        height: 60,
-        radius: 0,
-        color: Colors.transparent,
-        boxDecoration: BoxDecoration(
-          border: Border(top: BorderSide(color: Colors.grey.shade200)),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: List.generate(3, (index) {
-            final List<IconData> icons = [
-              Icons.person,
-              Icons.star,
-              Icons.add_circle,
-            ];
-            final List<String> labels = ['Home','Favorite','Create'];
-            return Consumer<NavbarProvider>(
-              builder: (context, navbarProvider, child) {
-                return CustomNavBar(
-                  index: index,
-                  onTap: (value) async {
-                    navbarProvider.getSelectedIndex(value!);
-                  },
-                  icon: icons[index],
-                  label: labels[index],
-                  size: navbarProvider.selectedIndex == index? 27:25,
-                  selectedColor: navbarProvider.selectedIndex == index
-                      ? brandSecondaryColor
-                      : Colors.grey,
-                );
-              },
-            );
-          }),
+      body: SizedBox(
+        height: SizeUtils.screenHeight,
+        width: SizeUtils.screenWidth,
+        child: Column(
+          children: [
+            CustomContainer(
+              margin: const EdgeInsets.only(top: 40),
+              padding: const EdgeInsets.only(left: 10,right: 05),
+              color: Colors.grey,
+              height: 50,
+              alignment: Alignment.centerLeft,
+              width: double.maxFinite,
+              boxDecoration: BoxDecoration(
+                  border:
+                  Border(bottom: BorderSide(color: Colors.grey.shade200))),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Scaffold.of(context).openDrawer();
+                    },
+                    icon: const Icon(
+                      Icons.menu,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  const CustomText(text: 'Contacts',textColor: Colors.grey,),
+                  IconButton(
+                    onPressed: () {
+                      showSearch(
+                        context: context,
+                        delegate: DataSearch(
+                            contactList: bodyProvider.contactList
+                        ),
+                      );
+                    },
+                    icon: const Icon(
+                      Icons.search,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: userService.contactInfoList!.isEmpty || userService.contactInfoList == null?  1 : userService.contactInfoList!.length,
+                itemBuilder: (context, index) {
+                  return userService.contactInfoList == null || userService.contactInfoList!.isEmpty ? const Center(child: CircularProgressIndicator()):
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 14,
+                      right: 14,
+                    ),
+                    child: AnimatedContainer(
+                      // key: Key(bodyProvider.contactList[index].name!),
+                      duration: const Duration(milliseconds: 300),
+                      padding: EdgeInsets.only(
+                        top: bodyProvider.contactList[index].isSelected ? 10: 0,
+                        bottom: bodyProvider.contactList[index].isSelected ? 10: 0,
+                      ),
+                      margin: EdgeInsets.only(
+                        bottom: bodyProvider.contactList[index].isSelected ? 10: 0,
+                        top: bodyProvider.contactList[index].isSelected ? 05: 0,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: bodyProvider.contactList[index].isSelected ? Colors.grey.shade200 : Colors.transparent,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+
+                      ),
+                      //height: bodyProvider.contactList[index].isSelected ? 120: null,
+                      child: Column(
+                        children: [
+                          ContactDetails(
+                            avatar: userService.contactInfoList?[index]['avatar'] != '' ? userService.contactInfoList![index]['avatar'] : userService.contactInfoList![index]['name'][0],
+                            name: userService.contactInfoList![index]['name'],
+                            number: userService.contactInfoList![index]['number'],
+                            secondBtnIcon: index.isEven? Icons.favorite_border: Icons.favorite,
+                            onTapOne: () {
+                              Navigator.pushNamed(context, '/detailsScreen');
+                            },
+                            onTapTwo: (){
+                              bodyProvider.getIsTrue(index);
+                            },
+                            secondBtn: (){
+                              const snackBar = SnackBar(
+                                padding: EdgeInsets.symmetric(horizontal: 10,vertical: 08),
+                                elevation: 0,
+                                duration: Duration(seconds: 1),
+                                backgroundColor: Colors.transparent,
+                                content: CustomSnackBar(
+                                  title: 'Added to the favorite',
+                                ),
+                              );
+                              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                            },
+                            call: (){},
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(
+                              left: bodyProvider.contactList[index].isSelected ? 0: 10,
+                              right: bodyProvider.contactList[index].isSelected ? 0: 10,
+                            ),
+                            child: const Divider(),
+                          ),
+                          if(bodyProvider.contactList[index].isSelected)
+                            IntrinsicHeight(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  IconAndTextBtn(
+                                    onTap: (){},
+                                    icon: Icons.message,
+                                    iconSize: 20,
+                                    label: 'Message',
+                                    labelSize: 12,
+                                  ),
+                                  const VerticalDivider(),
+                                  IconAndTextBtn(
+                                    onTap: (){
+                                      Navigator.pushNamed(context, '/detailsScreen');
+                                    },
+                                    icon: Icons.person,
+                                    iconSize: 20,
+                                    label: 'Details',
+                                    labelSize: 12,
+                                  ),
+                                  const VerticalDivider(),
+                                  IconAndTextBtn(
+                                    onTap: (){
+                                      showDialog(
+                                        context: context,
+                                        builder: (BuildContext context) {
+                                          return const CustomDialogBox();
+                                        },
+                                      );
+                                    },
+                                    icon: Icons.delete,
+                                    iconSize: 20,
+                                    label: 'Delete',
+                                    labelSize: 12,
+                                  ),
+                                ],
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
