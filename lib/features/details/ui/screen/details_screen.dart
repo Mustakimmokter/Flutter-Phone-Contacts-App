894a545a6e2628b76c7213c47a884098fd73091e
Mustakimokter <mustakimmokter@gmail.com>
2023-10-19 23:06:45 +0600

    Add more features

--- lib/features/details/ui/screen/details_screen.dart
+++ lib/features/details/ui/screen/details_screen.dart
@@ -1,21 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:phone_contact_app/shared/services/user_services/user.dart';
 import 'package:phone_contact_app/shared/utils/color_utils.dart';
 import 'package:phone_contact_app/shared/utils/size_utils.dart';
 import 'package:phone_contact_app/shared/widgets/index.dart';
+import 'package:provider/provider.dart';
+
+
+/*class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => UserService(),
+      child: DetailsScreenBody(
+        name: ,
+      ),
+    );
+  }
+}*/
+
+
+
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({
     Key? key,
     required this.name,
     required this.number,
+    required this.id
   }) : super(key: key);
 
-  final String name, number;
+  final String name, number,id;
 
   @override
   Widget build(BuildContext context) {
     SizeUtils().init(context);
     final TextEditingController numberCNTLR = TextEditingController(text: number);
     final TextEditingController nameCNTLR = TextEditingController(text: name);
+    final userService = Provider.of<UserService>(context);
     return Scaffold(
       backgroundColor: brandSecondaryColor,
       body: SizedBox(
@@ -28,7 +50,12 @@ class DetailsScreen extends StatelessWidget {
               top: 60,
               child: CustomBackBtn(
                 onTap: () {
-                  Navigator.pop(context);
+                  if(userService.isLoadAndUpdate){
+                    userService.getLoadAndUpdate(false);
+                    Navigator.pushNamedAndRemoveUntil(context, '/navBarController', (route) => false);
+                  }else{
+                    Navigator.pop(context);
+                  }
                 },
               ),
             ),
@@ -59,7 +86,7 @@ class DetailsScreen extends StatelessWidget {
                     borderColor: Colors.white,
                     borderWidth: 02,
                     child: CustomText(
-                      text: name[0],
+                      text: nameCNTLR.text[0],
                       textColor: Colors.white,
                       fontSize: 60,
                       fontWeight: FontWeight.w500,
@@ -69,14 +96,14 @@ class DetailsScreen extends StatelessWidget {
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: CustomText(
-                      text: name,
+                      text: nameCNTLR.text,
                       textAlign: TextAlign.center,
                       fontWeight: FontWeight.w500,
                       fontSize: 25,
                     ),
                   ),
                   CustomText(
-                    text: number,
+                    text: numberCNTLR.text,
                     textColor: Colors.grey,
                   ),
                 ],
@@ -86,38 +113,20 @@ class DetailsScreen extends StatelessWidget {
             Positioned(
               left: 5,
               right: 5,
-              top: 340,
+              top: 350,
               child: Column(
                 children: [
                   const Divider(),
                   const SizedBox(height: 15),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                     children : List.generate(3, (index) {
-                       final List<IconData> icons = [
-                         Icons.message,
-                         Icons.call,
-                         Icons.star_border,
-                       ];
-                       final List<String> label = [
-                         'Message',
-                         'Call',
-                         'Favorite'
-                       ];
-                       return CustomContainer(
-                         height: 60,
-                         width: 90,
-                         radius: 10,
-                         color: Colors.transparent,
-                         borderColor: Colors.grey,
-                         child: IconAndTextBtn(
-                           label: label[index],
-                           onTap: (){},
-                           icon: icons[index],
-                           color: brandColor,
-                         ),
-                       );
-                     }),
+                     children : [
+                       _rowAndButton('Message', Icons.message, () {}),
+                       _rowAndButton('call', Icons.call, () {
+                         userService.phoneCallDialer(number);
+                       }),
+                       _rowAndButton('Favorite', Icons.favorite_outline, () { }),
+                     ],
                   ),
                 ],
               ),
@@ -152,10 +161,14 @@ class DetailsScreen extends StatelessWidget {
                         numberChanged: (numberValue){
                           numberValue = numberCNTLR.text;
                         },
-                        title: 'Update',
+                        title: 'SAVE & CHANGE',
                         onPressed: (){
                           if(numberCNTLR.text.isNotEmpty && nameCNTLR.text.isNotEmpty){
-                            Navigator.pop(context);
+                            final updateData = {'name':nameCNTLR.text,'number':numberCNTLR.text};
+                            userService.updateContact(context, id, updateData: updateData);
+                            final params = {'name': nameCNTLR.text,'number':numberCNTLR.text,'id':id};
+                            userService.getLoadAndUpdate(true);
+                           Navigator.pushNamedAndRemoveUntil(context, '/detailsScreen', (route) => false,arguments: params);
                           }
                         },
                       ),
@@ -204,6 +217,22 @@ class DetailsScreen extends StatelessWidget {
 
           ],
         ),
+      ),
+    );
+  }
+  
+  Widget _rowAndButton(String label,IconData icon,VoidCallback onTap){
+    return CustomContainer(
+      height: 60,
+      width: 90,
+      radius: 10,
+      color: Colors.transparent,
+      borderColor: Colors.grey,
+      child: IconAndTextBtn(
+        label: label,
+        onTap: onTap,
+        icon: icon,
+        color: brandColor,
       ),
     );
   }
